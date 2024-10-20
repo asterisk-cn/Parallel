be6db1ce6c3fd2015f7de47a065c04572cf340c5
asterisk-cn <ttasuku-s122@g.ecc.u-tokyo.ac.jp>
2024-10-20 18:29:57 +0900

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private static readonly int MoveSpeed = Animator.StringToHash("moveSpeed");
+    [SerializeField] private float moveSpeed = 1f;
+    
+    private PlayerControls _playerControls;
+    private Vector2 _movement;
+    private Vector2 _lookPosition;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _camera = Camera.main;
+    }
+    
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+    
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        _movement = _playerControls.Movement.Move.ReadValue<Vector2>();
+        _lookPosition = _playerControls.Movement.Look.ReadValue<Vector2>();
+
+        _animator.SetFloat(MoveSpeed, _movement.magnitude);
+    }
+
+    private void Move()
+    {
+        _rb.MovePosition(_rb.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 playerScreenPoint = _camera.WorldToScreenPoint(transform.position);
+
+        _spriteRenderer.flipX = _lookPosition.x < playerScreenPoint.x;
+    }
+}
